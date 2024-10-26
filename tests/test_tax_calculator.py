97f1dfa4450691f42f5469788b701720446593d0
Ahereza01 <aherezavivian669@gmail.com>
2024-10-26 22:34:35 +0300

    another test passed

--- tests/test_tax_calculator.py
+++ tests/test_tax_calculator.py
@@ -9,3 +9,15 @@ class TestTaxCalculator(unittest.TestCase):
 
     def test_tax_between_12000_and_36000(self):
         self.assertEqual(calculate_tax(20000), 1600)  # Expected: 20% of 8,000
+
+    # test_tax_calculator.py
+
+class TestTaxCalculator(unittest.TestCase):
+    # Existing test
+    def test_no_tax_below_12000(self):
+        self.assertEqual(calculate_tax(10000), 0)
+
+    # New test
+    def test_tax_between_12000_and_36000(self):
+        self.assertEqual(calculate_tax(20000), 1600)  # 20% of 8,000
+
